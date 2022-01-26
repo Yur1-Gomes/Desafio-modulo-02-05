9213f982ca5b70fcd0b73d9e941f24fe03c778fa
Yur1-Gomes <yuri2912@hotmail.com>
2022-01-25 21:21:54 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let nomes = ["Maria","Jose","João","Eduardo","Rômulo"];
+console.log(nomes);
+
+nomes.unshift("Alessandro");
+console.log(nomes);
+
+nomes.pop();
+console.log(nomes);
+
+nomes.push("Willian","André");
+console.log(nomes);
+
+nomes.shift();
+console.log(nomes);
+
+const numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
+console.log(numbers);
+
